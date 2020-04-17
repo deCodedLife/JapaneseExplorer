1915b775ef2107f5c9ad9ad0a2792be4d6f425f6
deCodedLife <dcodedlife@gmail.com>
2020-04-17 12:47:02 +0300

    Design mainpage

--- lib/Pages/MainPage.dart
+++ lib/Pages/MainPage.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:japanese_explorer/Core/routeGenerator.dart';
 
 MaterialColor _theme = Colors.purple;
 
 class JapaneseExplorer extends StatelessWidget {
+  final List<String> cards = ['Recent','Update','Options'];
+
   @override
   Widget build(BuildContext context) {
     var materialApp = MaterialApp(
@@ -13,18 +14,53 @@ class JapaneseExplorer extends StatelessWidget {
           buttonColor: Colors.purple,
           buttonTheme: const ButtonThemeData(
             textTheme: ButtonTextTheme.primary,
-          )),
+          )
+      ),
       title: 'Japanese Explorer',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Mainpage'),
+          title: Text('Japanese explorer'),
+        ),
+        body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemCount: cards.length,
+          itemBuilder: (BuildContext context, int index) =>buildCard(context, index),
         ),
       ),
-      /*
-      initialRoute: '/',
-      onGenerateRoute: RouteGenerator.generateRoute,
-      */
     );
     return materialApp;
   }
+  Widget buildCard(BuildContext context, int index) {
+      return Card(
+        elevation: 2,
+        child: Container(
+          child: GestureDetector(
+            onTap: () {
+
+            },
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Spacer(),
+                  Icon(
+                    index == 0 ? Icons.history :
+                    index == 1 ? Icons.cloud_download :
+                    Icons.settings,
+                    size: 42.0,
+                  ),
+                  Spacer(),
+                  Text(
+                    cards.elementAt(index),
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
 }

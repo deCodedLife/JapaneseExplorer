811d2917c99b4990afade06725e625632959269a
deCodedLife <dcodedlife@gmail.com>
2020-04-23 21:13:52 +0300

    Start working with "StudyPage" and "CulturePage".

---
+++ lib/Pages/SubPages/CompareScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CompareScreen extends StatefulWidget {
+  @override
+  _CompareScreenState createState() => _CompareScreenState();
+}
+
+class _CompareScreenState extends State<CompareScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/Pages/SubPages/ExamScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ExamScreen extends StatefulWidget {
+  @override
+  _ExamScreenState createState() => _ExamScreenState();
+}
+
+class _ExamScreenState extends State<ExamScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
